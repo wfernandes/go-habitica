90d8a6e97f464f568dc6475c2f79009b06e055a4
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-04-02 12:55:05 -0600

    adds task crud integration test

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -5,8 +5,62 @@ import (
 	"os"
 	"testing"
 
+	. "github.com/onsi/gomega"
 	"github.com/wfernandes/go-habitica"
 )
+
+func TestIntegration_CRUDTask(t *testing.T) {
+	RegisterTestingT(t)
+	if testing.Short() {
+		t.Skip("skipping integration")
+	}
+
+	userID := os.Getenv("USER_ID")
+	apiToken := os.Getenv("API_TOKEN")
+
+	client, err := habitica.New(userID, apiToken)
+	Expect(err).ToNot(HaveOccurred(), "error creating client")
+
+	// create a task
+	ctx := context.Background()
+	taskResponse := &habitica.TaskResponse{}
+	task := &habitica.Task{
+		Text:  "This is a test task",
+		Notes: "Integration test task notes",
+		Type:  "todo",
+	}
+	taskResponse, err = client.Tasks.Create(ctx, task)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(taskResponse.Success).To(BeTrue(), "create did not succeed")
+	task = &taskResponse.Data
+	Expect(task.ID).ToNot(BeEmpty(), "task ID was empty")
+	Expect(task.UserID).To(Equal(userID))
+
+	//update the task
+	task.Text = "Task has been updated"
+	taskResponse, err = client.Tasks.Update(ctx, task.ID, task)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(taskResponse.Success).To(BeTrue(), "update did not succeed")
+
+	//get task
+	_, err = client.Tasks.Get(ctx, task.ID)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(task.Text).To(Equal("Task has been updated"))
+	Expect(task.UserID).To(Equal(userID))
+
+	//delete the task
+	taskResponse, err = client.Tasks.Delete(ctx, task.ID)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(taskResponse.Success).To(BeTrue(), "delete did not succeed")
+
+	// confirm that the task has been deleted
+	taskResponse, err = client.Tasks.Get(ctx, task.ID)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(taskResponse.Success).To(BeFalse())
+	Expect(taskResponse.Error).To(Equal("NotFound"))
+	Expect(taskResponse.Message).To(Equal("Task not found."))
+
+}
 
 func TestIntegration_UsersTasks(t *testing.T) {
 	if testing.Short() {
